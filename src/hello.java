cf50f38f07a83a8cb5c708643fd9eae6f4a5f40f
gaohan666 <1437810640@qq.com>
2021-02-04 15:26:13 +0800

    项目初始化

---
+++ src/hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+
+        String a = "asaa";
+       /* System.out.println(a);
+
+        System.out.println("hello");*/
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            scanner.next();
+            System.out.println("a");
+        }
+        scanner.close();
+
+        // int byte short long float double  char String boolean
+        
+    }
+}
+
---
+++ src/string.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class string {
+
+
+    public static void main(String[] args) {
+        /*int[] arr = {10, 20, 30, 40, 50};
+        for (int i : arr) {
+            System.out.println(i);
+        }*/
+
+        int[] a = {3, 2, 12, 489, 22, 42};
+        System.out.println(Arrays.toString(a));
+
+
+        
+        
+        //Arrays
+    }
+
+
+
+
+}
